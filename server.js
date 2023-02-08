1741ac4f254053397bb9528e84ddeae14e3a0dd0
Alexander Príncipe Tolentino <principetolentinoa@hotmail.com>
2023-02-08 09:48:31 -0500

    sigue sin funcionar

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const config = require('./api/config/config');
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
+const port = process.env.port || 3000;
 
 const server = http.createServer(app);
 
